13b1912d91dc99af6aefcf2af74d52a399f472f4
Damian Kowalik <dev.damian.kowalik@gmail.com>
2019-05-26 16:35:54 +0200

    Add the ability to add todos in AddTodoForm.js

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,9 +1,46 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { ADD_TODO } from './reducers'
 
 class AddTodoForm extends Component {
+  state = {
+    input: ''
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    this.props.addTodo(this.state.input)
+    this.setState({ input: '' })
+  }
+
+  handleChange = e => {
+    this.setState({
+      input: e.target.value
+    })
+  }
+
   render() {
-    return null
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.input}
+          onChange={this.handleChange}
+        />
+      </form>
+    )
   }
 }
 
-export default AddTodoForm
+const mapDispatchToProps = dispatch => {
+  return {
+    addTodo: body => {
+      dispatch({ type: ADD_TODO, body })
+    }
+  }
+}
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(AddTodoForm)
