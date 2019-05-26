1fef5c307765b8dfa9b01fd747d9d3a4b68a625c
Damian Kowalik <dev.damian.kowalik@gmail.com>
2019-05-26 19:01:00 +0200

    Populate VisiblityFilters.js with buttons

--- src/VisibilityFilters.js
+++ src/VisibilityFilters.js
@@ -1,5 +1,11 @@
 import React from 'react'
 
-const VisibilityFilters = () => null
+const VisibilityFilters = () => (
+  <div>
+    <button type="button">Show All</button>
+    <button type="button">Show Completed</button>
+    <button type="button">Show Not Completed</button>
+  </div>
+)
 
 export default VisibilityFilters
