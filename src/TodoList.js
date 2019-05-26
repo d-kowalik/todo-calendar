841bdcf322737d35ea3424fb0dc7d6e1327fdd78
Damian Kowalik <dev.damian.kowalik@gmail.com>
2019-05-26 18:19:16 +0200

    Implement toggling todo item as completed or not on click

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { toggleTodo } from './actions'
 
 class TodoList extends Component {
   render() {
     return (
       <ul>
         {this.props.todos.map(todo => (
-          <li key={todo.id}>{todo.body}</li>
+          <li key={todo.id} onClick={() => this.props.toggleTodo(todo.id)}>
+            {todo.body}
+          </li>
         ))}
       </ul>
     )
@@ -19,4 +22,15 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps)(TodoList)
+const mapDispatchToProps = dispatch => {
+  return {
+    toggleTodo: id => {
+      dispatch(toggleTodo(id))
+    }
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList)
