6f55299714bd66bde63b20bfee0003c93a2fb368
Damian Kowalik <dev.damian.kowalik@gmail.com>
2019-05-26 16:24:54 +0200

    Wrap App component in a Provider

--- src/index.js
+++ src/index.js
@@ -5,10 +5,16 @@ import App from './App'
 import * as serviceWorker from './serviceWorker'
 import { createStore } from 'redux'
 import { todoReducer } from './reducers'
+import { Provider } from 'react-redux'
 
 const store = createStore(todoReducer)
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
