72a80ac7dee36c7e3ced8cf725ed9b4c71d0336e
Damian Kowalik <dev.damian.kowalik@gmail.com>
2019-05-26 19:03:00 +0200

    Implement filter action creator and filters

--- src/actions.js
+++ src/actions.js
@@ -1,5 +1,12 @@
 export const ADD_TODO = 'ADD_TODO'
 export const TOGGLE_TODO = 'TOGGLE_TODO'
+export const CHANGE_FILTER = 'CHANGE_FILTER'
+
+export const Filters = {
+  SHOW_ALL: 'SHOW_ALL',
+  SHOW_COMPLETD: 'SHOW_COMPLETED',
+  SHOW_NOT_COMPLETED: 'SHOW_NOT_COMPLETED'
+}
 
 export function addTodo(body) {
   return {
@@ -12,5 +19,12 @@ export function toggleTodo(id) {
   return {
     type: TOGGLE_TODO,
     id
+  }
+}
+
+export function changeFilter(filter) {
+  return {
+    type: CHANGE_FILTER,
+    filter
   }
 }
