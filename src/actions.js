71694acaabcc2f5414c84fea67bce67df8d59c01
Damian Kowalik <dev.damian.kowalik@gmail.com>
2019-05-26 18:10:24 +0200

    Add TOGGLE_TODO action

--- src/actions.js
+++ src/actions.js
@@ -1,8 +1,16 @@
 export const ADD_TODO = 'ADD_TODO'
+export const TOGGLE_TODO = 'TOGGLE_TODO'
 
 export function addTodo(body) {
   return {
     type: ADD_TODO,
     body
+  }
+}
+
+export function toggleTodo(id) {
+  return {
+    type: TOGGLE_TODO,
+    id
   }
 }
