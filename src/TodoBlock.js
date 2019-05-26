f6c400216086805bf9591a4a8a45aa6c557ffbeb
Damian Kowalik <dev.damian.kowalik@gmail.com>
2019-05-26 16:15:34 +0200

    Make TodoBlock component render AddTodoForm, TodoList and VisibilityFilters

--- src/TodoBlock.js
+++ src/TodoBlock.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react'
+import AddTodoForm from 'AddTodoForm'
+import TodoList from 'TodoList'
+import VisibilityFilters from 'VisibilityFilters'
 
 class TodoBlock extends Component {
   render() {
-    return null
+    return (
+      <div>
+        <AddTodoForm />
+        <TodoList />
+        <VisibilityFilters />
+      </div>
+    )
   }
 }
 
