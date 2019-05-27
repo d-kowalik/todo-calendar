5384c51a87e7726d037131575d89cd317785d160
Damian Kowalik <dev.damian.kowalik@gmail.com>
2019-05-27 20:55:08 +0200

    Add currently selectedDate to rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,16 @@
 import { combineReducers } from 'redux'
 import { filter } from './filterReducer'
 import { todos } from './todosReducer'
+import { assembleDate } from '../dateHelper'
+
+const date = assembleDate(new Date())
+
+const selectedDate = (state = date, action) => {
+  return state
+}
 
 export const rootReducer = combineReducers({
   todos,
-  filter
+  filter,
+  selectedDate
 })
