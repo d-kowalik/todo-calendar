c8caac0216068d9b3ff59d015a6dc2f9aa3bd133
Damian Kowalik <dev.damian.kowalik@gmail.com>
2019-05-28 15:00:59 +0200

    Implement moving day backwards and forwards

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import '../styles/App.css'
 import TodoBlock from './TodoBlock'
+import { connect } from 'react-redux'
+import { reverseDay, advanceDay } from '../actions'
 
-function App() {
+function App(props) {
   return (
     <div className="App">
+      <button onClick={props.previousDay}>Previous Day</button>
       <TodoBlock />
+      <button onClick={props.nextDay}>Next Day</button>
     </div>
   )
 }
 
-export default App
+const mapDispatchToProps = dispatch => {
+  return {
+    previousDay: () => dispatch(reverseDay()),
+    nextDay: () => dispatch(advanceDay())
+  }
+}
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(App)
