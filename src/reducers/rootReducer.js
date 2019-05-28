75a0d1aae76b0d8b2133ecee11e725bc655c6864
Damian Kowalik <dev.damian.kowalik@gmail.com>
2019-05-28 15:00:47 +0200

    Fix advancing and reversing date

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -11,12 +11,14 @@ const selectedDate = (state = date, action) => {
   switch (action.type) {
     case ADVANCE_DAY:
       selectedDate = dateStringToDate(state)
-      const tomorrow = new Date(selectedDate.getDate() + 1)
+      const tomorrow = new Date()
+      tomorrow.setDate(selectedDate.getDate() + 1)
       return assembleDate(tomorrow)
 
     case REVERSE_DAY:
       selectedDate = dateStringToDate(state)
-      const yesterday = new Date(selectedDate.getDate() - 1)
+      const yesterday = new Date()
+      yesterday.setDate(selectedDate.getDate() - 1)
       return assembleDate(yesterday)
     default:
       return state
