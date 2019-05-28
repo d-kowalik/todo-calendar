ec273a269c6fa8c49ac057694ebf6a63cd83dcdf
Damian Kowalik <dev.damian.kowalik@gmail.com>
2019-05-28 14:30:41 +0200

    Implement actions for advancing and reversing the day

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,11 +1,26 @@
 import { filter } from './filterReducer'
 import { todos } from './todosReducer'
-import { today } from '../dateHelper'
+import { today, dateStringToDate, assembleDate } from '../dateHelper'
+
+import { ADVANCE_DAY, REVERSE_DAY } from '../actions'
 
 const date = today()
 
 const selectedDate = (state = date, action) => {
-  return state
+  let selectedDate
+  switch (action.type) {
+    case ADVANCE_DAY:
+      selectedDate = dateStringToDate(state)
+      const tomorrow = new Date(selectedDate.getDate() + 1)
+      return assembleDate(tomorrow)
+
+    case REVERSE_DAY:
+      selectedDate = dateStringToDate(state)
+      const yesterday = new Date(selectedDate.getDate() - 1)
+      return assembleDate(yesterday)
+    default:
+      return state
+  }
 }
 
 export const rootReducer = (state = {}, action) => {
