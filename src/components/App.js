5b26d09c2868039b0e5fa326893248c9fbe4878f
Damian Kowalik <dev.damian.kowalik@gmail.com>
2019-05-31 16:33:41 +0200

    Handle switching days by swiping left or right

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import '../styles/App.css'
 import ShadowTodoBlock from './ShadowTodoBlock'
 import TodoBlock from './TodoBlock'
@@ -11,24 +11,67 @@ import {
   dateStringToDate
 } from '../dateHelper'
 
-function App(props) {
-  return (
-    <div className="App">
-      {window.screen.availWidth > 600 ? (
-        <ShadowTodoBlock
-          date={assembleDate(reverseDateByDay(dateStringToDate(props.date)))}
-          onClick={props.previousDay}
+class App extends Component {
+  state = {
+    initialPositionX: 0,
+    currentPositionX: 0
+  }
+
+  handleSwipeStart = event => {
+    event.persist()
+    console.log(event)
+
+    this.setState({ initialPositionX: event.clientX })
+  }
+
+  handleSwipeMove = event => {
+    this.setState({ currentPositionX: event.clientX })
+  }
+
+  handleSwipeEnd = event => {
+    const res = this.state.initialPositionX - this.state.currentPositionX
+    if (res < -100) {
+      this.props.nextDay()
+    } else if (res > 100) {
+      this.props.previousDay()
+    }
+  }
+
+  render() {
+    return (
+      <div
+        className="App"
+        onMouseDown={this.handleSwipeStart}
+        onMouseMove={this.handleSwipeMove}
+        onMouseUp={this.handleSwipeEnd}
+        onTouchStart={this.handleSwipeStart}
+        onTouchMove={this.handleSwipeMove}
+        onTouchEnd={this.handleSwipeEnd}
+      >
+        {window.screen.availWidth > 600 ? (
+          <ShadowTodoBlock
+            date={assembleDate(
+              reverseDateByDay(dateStringToDate(this.props.date))
+            )}
+            onClick={this.props.previousDay}
+          />
+        ) : null}
+        <TodoBlock
+          onMouseDown={this.handleSwipeStart}
+          onMouseMove={this.handleSwipeMove}
+          onMouseUp={this.handleSwipeEnd}
         />
-      ) : null}
-      <TodoBlock />
-      {window.screen.availWidth > 600 ? (
-        <ShadowTodoBlock
-          date={assembleDate(advanceDateByDay(dateStringToDate(props.date)))}
-          onClick={props.nextDay}
-        />
-      ) : null}
-    </div>
-  )
+        {window.screen.availWidth > 600 ? (
+          <ShadowTodoBlock
+            date={assembleDate(
+              advanceDateByDay(dateStringToDate(this.props.date))
+            )}
+            onClick={this.props.nextDay}
+          />
+        ) : null}
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = state => {
