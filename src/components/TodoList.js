8f635752d8f8a5abf47694302c2b51c556849408
Damian Kowalik <dev.damian.kowalik@gmail.com>
2019-05-31 12:24:50 +0200

    Add missing keys in TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,7 +5,7 @@ import '../styles/TodoList.css'
 const TodoList = ({ todos }) => (
   <ul className="TodoList">
     {todos.map(todo => {
-      return <TodoContainer todo={todo} />
+      return <TodoContainer key={todo.id} todo={todo} />
     })}
   </ul>
 )
