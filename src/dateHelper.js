fd6e6ced2c194c899ff5af9f1872e0fc675dd9f3
Damian Kowalik <dev.damian.kowalik@gmail.com>
2019-05-31 12:48:42 +0200

    Fix advanceDateByDay and reverseDateByDay not  working correctly

--- src/dateHelper.js
+++ src/dateHelper.js
@@ -1,6 +1,6 @@
 export function assembleDate(date) {
   let day = date.getDate()
-  let month = date.getMonth()
+  let month = date.getMonth() + 1
   let year = date.getFullYear()
   let weekdays = [
     'Sunday',
@@ -18,21 +18,57 @@ export function dateStringToDate(string) {
   const datePart = string.split(', ')
   const dateComponents = datePart[1].split('-')
   const year = dateComponents[2]
-  const month = dateComponents[1]
+  const month = dateComponents[1] - 1
   const day = dateComponents[0]
+
   return new Date(year, month, day)
 }
 
 export const advanceDateByDay = date => {
-  const tomorrow = new Date()
-  tomorrow.setDate(date.getDate() + 1)
-  return tomorrow
+  return addDays(date, 1)
+}
+
+// addDays function courtesy to StackOverflow user Clever Human, https://stackoverflow.com/a/5757691
+function addDays(date, amount) {
+  var tzOff = date.getTimezoneOffset() * 60 * 1000,
+    t = date.getTime(),
+    d = new Date(),
+    tzOff2
+
+  t += 1000 * 60 * 60 * 24 * amount
+  d.setTime(t)
+
+  tzOff2 = d.getTimezoneOffset() * 60 * 1000
+  if (tzOff !== tzOff2) {
+    var diff = tzOff2 - tzOff
+    t += diff
+    d.setTime(t)
+  }
+
+  return d
+}
+
+function removeDays(date, amount) {
+  var tzOff = date.getTimezoneOffset() * 60 * 1000,
+    t = date.getTime(),
+    d = new Date(),
+    tzOff2
+
+  t -= 1000 * 60 * 60 * 24 * amount
+  d.setTime(t)
+
+  tzOff2 = d.getTimezoneOffset() * 60 * 1000
+  if (tzOff !== tzOff2) {
+    var diff = tzOff2 - tzOff
+    t += diff
+    d.setTime(t)
+  }
+
+  return d
 }
 
 export const reverseDateByDay = date => {
-  const tomorrow = new Date()
-  tomorrow.setDate(date.getDate() - 1)
-  return tomorrow
+  return removeDays(date, 1)
 }
 
 export const today = () => assembleDate(new Date())
