e17ead6dc19451ea58df31889ea80e268f3c198f
Damian Kowalik <dev.damian.kowalik@gmail.com>
2019-06-09 20:16:20 +0200

    Add monthSelected state element to Redux

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,12 +1,24 @@
 import { filter } from './filterReducer'
 import { todosByDate } from './todosByDateReducer'
 import { selectedDate } from './selectedDateReducer'
+import { TOGGLE_MONTH_SELECTED } from '../actions'
+
+function monthSelected(state = false, action) {
+  switch (action.type) {
+    case TOGGLE_MONTH_SELECTED:
+      return !state
+    default:
+      return state
+  }
+}
 
 export const rootReducer = (state = {}, action) => {
   const date = selectedDate(state.selectedDate, action)
+  const month = monthSelected(state.monthSelected, action)
   return {
     todosByDate: todosByDate(state.todosByDate, action, date),
     filter: filter(state.filter, action),
-    selectedDate: date
+    selectedDate: date,
+    monthSelected: month
   }
 }
