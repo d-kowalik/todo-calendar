e291725570bb58f0a984c135cebde5e251bf7eb9
Damian Kowalik <dev.damian.kowalik@gmail.com>
2019-06-09 21:50:29 +0200

    Change monthSelected to isMonthSelected state element

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -3,7 +3,7 @@ import { todosByDate } from './todosByDateReducer'
 import { selectedDate } from './selectedDateReducer'
 import { TOGGLE_MONTH_SELECTED } from '../actions'
 
-function monthSelected(state = false, action) {
+function isMonthSelected(state = false, action) {
   switch (action.type) {
     case TOGGLE_MONTH_SELECTED:
       return !state
@@ -14,11 +14,11 @@ function monthSelected(state = false, action) {
 
 export const rootReducer = (state = {}, action) => {
   const date = selectedDate(state.selectedDate, action)
-  const month = monthSelected(state.monthSelected, action)
+  const isMonthSelectedCpy = isMonthSelected(state.monthSelected, action)
   return {
     todosByDate: todosByDate(state.todosByDate, action, date),
     filter: filter(state.filter, action),
     selectedDate: date,
-    monthSelected: month
+    isMonthSelected: isMonthSelectedCpy
   }
 }
